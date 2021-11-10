037d4fa92911711a34ee4bc30b08997dc086de73
N. Martin Abregu <nmabregu@gmail.com>
2021-11-10 08:51:28 -0300

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Mark' },
@@ -23,5 +26,25 @@ app.get('/users/:id', function (req, res) {
     });
 });
 
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+ 
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+ 
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+});
 
 app.listen(8000, () => console.log('server is listening'));
